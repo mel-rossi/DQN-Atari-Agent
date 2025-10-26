cb2a4c5b7023b602c04ffbc4a17917799474d7f7
mel-rossi <melani.r.rodrigues@gmail.com>
2025-10-25 22:41:04 -0700

    Create AtariProcessing class + __init__ function

--- src/atari_processing.py
+++ src/atari_processing.py
@@ -2,4 +2,28 @@
 # Takes raaw Atari frames and makes them suitable for neural network: 
 # converts to grayscale, resizes to 84x84, stacks 4 frames together (motion),
 # does frame skipping (repeats actions), and adds random no-ops at the start. 
-# Makes the raw game playable by agent. 
+# Makes the raw game playable by agent.
+import gymnasium as gym
+import numpy as np 
+import cv2 
+
+# Wrapper that handles core image preprocessing 
+class AtariProcessing(gym.Wrapper): 
+    """
+    Atari 2600 preprocessing
+    - Frame skipping (repeat action for 4 frames) 
+    - Max pooling over last 2 frames
+    - Grayscale conversion
+    - Resize to 84x84
+    """
+
+    def __init__(self, env, frame_skip = 4, screen_size = 84): 
+        super().__init(env)
+        self.frame_skip = frame_skip
+        self.screen_size = screen_size
+
+        # Buffers for max pooling 
+        self.obs_buffer = [
+            np.zeros((84, 84), dtype=np.vint8),
+            np.zeros((84, 84), dtype=np.vint8)
+        ]
