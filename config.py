fc7508a088cb3059f9c20ac8265675291dfc8bc7
angcortes <angelcortes@tutanota.com>
2025-10-25 17:08:37 -0700

    restructured the folders

--- config.py
+++ config.py
@@ -3,3 +3,33 @@
 # and 'heavy' (serious long training). 
 # Contains all hyperparameters like learning rate, epsilon decay, replay buffer size, etc.
 # Makes it easy to switch between different training setups without editing code. 
+
+
+
+# config.py
+# File to store all project hyperparameters and settings
+import torch
+
+# Environment and Paths 
+ENV_ID = "PongNoFrameskip-v4"
+LOG_DIR = "./tensorboard_logs/"
+MODEL_DIR = "./models/"
+BEST_MODEL_PATH = f"{MODEL_DIR}/dqn_pong_best_model.zip"
+
+# 2. Device 
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+
+#  3. Training Hyperparameters
+TOTAL_TIMESTEPS = 2_000_000
+LEARNING_RATE = 1e-4
+BUFFER_SIZE = 1_000_000
+LEARNING_STARTS = 100_000
+BATCH_SIZE = 32
+GAMMA = 0.99
+TRAIN_FREQ = 4
+GRADIENT_STEPS = 1
+TARGET_UPDATE_INTERVAL = 1000
+
+# 4. Evaluation 
+EVAL_FREQ = 10_000
+N_EVAL_EPISODES = 10
--- src/train.py
+++ src/train.py
@@ -1,4 +1,35 @@
-# Main training script. Run to train agent. 
-# Creates the game enviroment, and DQN agent, then runs thousands of game episodes
-# where teh agent learns by playing. 
-# Saves checkpoints every 500 episodes and graphs to show how well it's learning.
+import gymnasium as gym
+import ale_py  # Registers the Atari environments
+import torch
+import os
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
+
+# --- 1. Configuration & Hyperparameters ---
+# Environment and Paths
+ENV_ID = "PongNoFrameskip-v4"
+LOG_DIR = "./tensorboard_logs/"
+MODEL_DIR = "./models/"
+BEST_MODEL_PATH = f"{MODEL_DIR}/dqn_pong_best_model.zip"
+
+# Device
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+
+# Training Hyperparameters
+TOTAL_TIMESTEPS = 2_000_000
+LEARNING_RATE = 1e-4
+BUFFER_SIZE = 1_000_000
+LEARNING_STARTS = 100_000
+BATCH_SIZE = 32
+GAMMA = 0.99
+TRAIN_FREQ = 4
+GRADIENT_STEPS = 1
+TARGET_UPDATE_INTERVAL = 1000
+
+# Evaluation
+EVAL_FREQ = 10_000
+N_EVAL_EPISODES = 10
