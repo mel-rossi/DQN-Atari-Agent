ff15f4c52b6a747593ace30e1e2a9c9ece21a814
mel-rossi <melani.r.rodrigues@gmail.com>
2025-10-27 16:10:52 -0700

    Delete unnecessary files

--- mel_src/dqn_agent.py
+++
@@ -1,4 +0,0 @@
-# Contains DQN agent logic - 
-# Decides which actions to take (epsilon-greedy), stores memories of what happened, 
-# and trains itself by replaying memories. 
-# Two neural networks: one for learning (online) and one for stability (target).
--- train_with_config.py
+++
@@ -1,4 +0,0 @@
-# Training script that uses the config file. 
-# Version of train.py that lets you pick a config present from command line. 
-# Flexible for running lots of experiments. 
-# Use this instead of train.py if you want easy configuration switching.
