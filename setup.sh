78077801d1794472196ced0f6154893ba70f53c7
mel-rossi <melani.r.rodrigues@gmail.com>
2025-10-29 11:30:04 -0700

    Fix typo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
+rm -rf venv
 python3 -m venv venv # Create virtual environment 
 source venv/bin/activate # Activate virtual environment
 pip uninstall gym -y # Uninstall old gym package 
-pip install -r requirements.tx # Install requirements
+pip install -r requirements.txt # Install requirements
