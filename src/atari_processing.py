35d3961830738900e604e25be3245d126d5337e4
mel-rossi <melani.r.rodrigues@gmail.com>
2025-10-28 17:58:32 -0700

    Clean up files/folders

--- src/atari_processing.py
+++
@@ -1,5 +0,0 @@
-# Wraps the game enviroment to preprocess frames. 
-# Takes raaw Atari frames and makes them suitable for neural network: 
-# converts to grayscale, resizes to 84x84, stacks 4 frames together (motion),
-# does frame skipping (repeats actions), and adds random no-ops at the start. 
-# Makes the raw game playable by agent. 
